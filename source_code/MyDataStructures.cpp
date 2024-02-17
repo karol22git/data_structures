99b2935ec64e6fceab72b18028b16966754e23d2
karol22git <kamel.poczta676@gmail.com>
2024-02-17 12:19:46 +0100

    Removing source code from header fiel

--- source_code/MyDataStructures.cpp
+++ source_code/MyDataStructures.cpp
@@ -1,4 +1,4 @@
-#include "MyDataStructures.h"
+#include "MyDataStructures.hpp"
 
 
 template <typename T>
@@ -38,7 +38,7 @@ template bool MyDataStructures::LinkedList::Contains<int>(struct node<int> *root
 
 template <typename T>
 void MyDataStructures::LinkedList::DeleteNode(struct node<T>* root,T value) {
-    struct node<T> tmp = root;
+    struct node<T>* tmp = root;
     if(root->value == value) {
         root = root->next;
     }
@@ -54,7 +54,7 @@ template  void MyDataStructures::LinkedList::DeleteNode<int>(struct node<int>* r
 
 template <typename T>
 struct node<T>* MyDataStructures::LinkedList::LinkedListInit(T value) {
-    struct node<T>* new_node;
+    struct node<T>* new_node = new struct node<T>;
     new_node->value = value;
     new_node->next = nullptr;
     return new_node;
